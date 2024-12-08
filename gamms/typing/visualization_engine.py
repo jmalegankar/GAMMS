ac0b88327617e66de690acfe11ce31ca7ddc6945
Xiao Jiang <bjjx1999@live.com>
2024-12-07 21:04:59 -0800

    Add visualization engine abstraction

---
+++ gamms/typing/visualization_engine.py
@@ -0,0 +1,65 @@
+from typing import Callable
+from abc import ABC, abstractmethod
+
+class VisualizationEngine(ABC):
+    """
+    The abstract interface for the visualization engine.
+    """
+
+    tick_callback: Callable[[float]]
+    """
+    This will be called every tick of the game loop. You can use it for tick callback for other systems.
+
+    Args:
+        delta_time (float): The time in seconds since the last tick.
+    """
+
+    will_quit: bool
+    """
+    This will be set to True if the user tries to quit the game.
+    """
+
+    def __init__(self, tick_callback: Callable[[float]]):
+        super().__init__()
+
+        self.tick_callback: Callable[[float]] = tick_callback
+        self.will_quit: bool = False
+
+    def run_game_loop(self):
+        """
+        This is a basic game loop, can override in implementation.
+        """
+        while not self.will_quit:
+            self.handle_input()
+            self.handle_tick()
+            self.handle_render()
+
+        self.cleanup()
+
+    @abstractmethod
+    def handle_input(self):
+        """
+        This will be called every tick of the game loop, used to handle all input.
+        """
+        pass
+
+    @abstractmethod
+    def handle_tick(self):
+        """
+        This should handle all internal logic of visualization engine, as well as call the tick_callback.
+        """
+        pass
+
+    @abstractmethod
+    def handle_render(self):
+        """
+        This will be called every tick of the game loop, used to handle all rendering.
+        """
+        pass
+
+    @abstractmethod
+    def cleanup(self):
+        """
+        This will be called when the game loop exits. It should be used to clean up any resources that were used during the game loop.
+        """
+        pass
