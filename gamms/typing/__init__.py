65bfcfbf768748dcfec4be801cbb9e4ff156139e
bridgesign <eyeoeternity@gmail.com>
2024-12-11 18:13:34 +0000

    Computation Engine tested

--- gamms/typing/__init__.py
+++ gamms/typing/__init__.py
@@ -1,4 +1,4 @@
-from gamms.typing.compute_engine import IComputeEngine
+from gamms.typing.compute_engine import IComputeEngine, ITask
 from gamms.typing.memory_engine import IMemoryEngine
 from gamms.typing.message_engine import IMessageEngine
 from gamms.typing.internal_context import IInternalContext
