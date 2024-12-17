27b7a5dfc477605cd2a5ef12661e4425cacea290
bridgesign <eyeoeternity@gmail.com>
2024-12-17 22:57:01 +0000

    osm separation

--- examples/base/create_graph.py
+++ examples/base/create_graph.py
@@ -1,13 +1,17 @@
 from config import location, graph_path
-from gamms.GraphEngine.graph_engine import GraphEngine
+
+import gamms
+import gamms.osm
 
 # Create a graph
-graph_egine = GraphEngine()
+# graph_egine = GraphEngine()
 # graph_egine.create_graph(location)
 # graph_egine.graph.visualize()
 # graph_egine.graph.save(graph_path)
 
+G = gamms.osm.create_osm_graph(location)
 
+print(G.edges(data=True))
 
-graph_egine.load(graph_path)
-graph_egine.graph.visualize()
+# graph_egine.load(graph_path)
+# graph_egine.graph.visualize()
